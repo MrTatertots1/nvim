dc67b5c8dd85d3087f412f90705c3ebffa1d765e
Aaron Anderson <mrtatertots1@outlook.com>
2025-12-18 02:47:08 -0700

    Refactor mason "ensure-installed" and use Roslyn instead of Omnisharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,46 @@
 return {
   -- LSP and Mason
   { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim", build = ":MasonUpdate" },
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "lua-language-server",
+        -- "lua_ls",
+
+        "xmlformatter",
+        "csharpier",
+        "prettier",
+
+        "stylua",
+        "bicep-lsp",
+        "html-lsp",
+        "css-lsp",
+        "eslint-lsp",
+        "typescript-language-server",
+        "json-lsp",
+        "rust-analyzer",
+
+        -- C#
+        "roslyn",
+        -- "rzls",
+        -- "csharp-language-server",
+        -- "omnisharp",
+      }
+    }
+  },
   { "williamboman/mason-lspconfig.nvim" },
+  {
+    "seblyng/roslyn.nvim",
+    ft = { "cs" },
+    opts = {
+      -- NOTE: leave empty to use defaults
+      -- Custom configuration here
+    }
+  },
 }
